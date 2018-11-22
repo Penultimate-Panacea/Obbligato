93c153f470b2849850cfdafb5c4b1c4f405ae8a0
Rian <Penultimate-Panacea@users.noreply.github.com>
2018-11-22 02:01:56 -0500

    It exits with code 0 despite supposed to be 3?!

--- main.cpp
+++ main.cpp
@@ -6,39 +6,31 @@
 #include "Seeder.h"
 #include "BaseEightyNine.h"
 
-void loggingStart(){
-    //freopen("output2.csv", "w", stdout);
-    freopen("debug.txt", "w", stderr);
-}
-/*
-int test(int argc, char* argv[]) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
-*/
 
 int main() {
-    loggingStart();
+    freopen("debug.log", "w", stderr);
     Seeder testSeeder(0,0,0,0,0,1);
     rlcg::ReversibleLCG reRngTone(testSeeder.getSeed());
     //TODO make better toStream and validate output files match seed
     std::stringstream sstream;
     sstream << testSeeder;
     std::string location = sstream.str();
-
+    std::cerr << location;
     freopen("output.tone", "w", stdout);
     for (int i = 0; i < 3201; ++i) {
         std::cout << BaseEightyNine(reRngTone.next()).firstChar();
     }
     fclose(stdout);
+    std::cout << "test";
     rlcg::ReversibleLCG reRngTime(testSeeder.getSeed());
     freopen("output.time", "w", stdout);
     for (int i = 0; i < 3201; ++i){
         std::cout << (reRngTime.next() % 10);
     }
     fclose(stdout);
+    std::cout << "test";
     ABCFilemaker ABC("output.tone", "output.time", location);
     ABC.makeABCfile();
-    return 0;
+    return 3;
 }
 
